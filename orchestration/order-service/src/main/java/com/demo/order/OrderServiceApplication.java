e9b2a26158568ca602b22fb45e7f9ac4751346bc
banghanam02 <banghanam02@gmail.com>
2026-05-31 16:06:43 +0700

    feat:Commit jenkins

--- orchestration/order-service/src/main/java/com/demo/order/OrderServiceApplication.java
+++ orchestration/order-service/src/main/java/com/demo/order/OrderServiceApplication.java
@@ -5,8 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
-        System.out.println("hello");
+
+        System.out.println("Random Key: " +
+                RandomStringGenerator.generate(16));
     }
 }
