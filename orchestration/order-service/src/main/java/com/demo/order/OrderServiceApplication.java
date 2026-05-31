365a0d0d688acf09cce7e71c15e6c6e916a95d0d
banghanam02 <banghanam02@gmail.com>
2026-05-31 16:52:12 +0700

    feat:CrashLoopBackOff

--- orchestration/order-service/src/main/java/com/demo/order/OrderServiceApplication.java
+++ orchestration/order-service/src/main/java/com/demo/order/OrderServiceApplication.java
@@ -1,22 +1,27 @@
 package com.demo.order;
 
-import org.springframework.boot.CommandLineRunner;
+//import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
+
+        System.out.println("Random Key: " +
+                RandomStringGenerator.generate(16));
+        System.out.println("HELLO1111111111111111111111111111111111111111111111");
+        System.out.println("123TEST");
     }
 
-    @Bean
-    CommandLineRunner crashApp() {
-        return args -> {
-            System.out.println("Application started...");
-            throw new RuntimeException("Crash for testing");
-        };
-    }
+//    @Bean
+//    CommandLineRunner crashApp() {
+//        return args -> {
+//            System.out.println("Application started...");
+//            throw new RuntimeException("Crash for testing");
+//        };
+//    }
 }
